bbab97bf7e1352cf6d0279bc788489340b1f2dbf
Basil Crow <me@basilcrow.com>
2023-09-14 13:23:08 -0700

    Refresh plugin for September 2023

--- src/test/java/com/amadeus/jenkins/plugins/workflow/libs/HttpRetrieverIntegrationTests.java
+++ src/test/java/com/amadeus/jenkins/plugins/workflow/libs/HttpRetrieverIntegrationTests.java
@@ -1,5 +1,8 @@
 package com.amadeus.jenkins.plugins.workflow.libs;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.hasSize;
+
 import com.cloudbees.plugins.credentials.CredentialsScope;
 import com.cloudbees.plugins.credentials.SystemCredentialsProvider;
 import com.cloudbees.plugins.credentials.impl.UsernamePasswordCredentialsImpl;
@@ -8,8 +11,10 @@ import com.github.tomakehurst.wiremock.core.WireMockConfiguration;
 import com.github.tomakehurst.wiremock.http.RequestMethod;
 import com.github.tomakehurst.wiremock.junit.WireMockRule;
 import com.github.tomakehurst.wiremock.matching.MatchResult;
+import edu.umd.cs.findbugs.annotations.NonNull;
+import edu.umd.cs.findbugs.annotations.Nullable;
 import hudson.ExtensionList;
-import hudson.FilePath;
+import hudson.Functions;
 import org.apache.commons.io.IOUtils;
 import org.apache.http.HttpHeaders;
 import org.jenkinsci.plugins.workflow.cps.CpsFlowDefinition;
@@ -18,16 +23,19 @@ import org.jenkinsci.plugins.workflow.job.WorkflowRun;
 import org.jenkinsci.plugins.workflow.libs.GlobalLibraries;
 import org.jenkinsci.plugins.workflow.libs.LibraryConfiguration;
 import org.junit.Assert;
+import org.junit.Assume;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.jvnet.hudson.test.JenkinsRule;
 
-import javax.annotation.Nonnull;
-import javax.annotation.Nullable;
 import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
 import java.util.Objects;
 import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class HttpRetrieverIntegrationTests {
     @Rule
@@ -54,104 +62,104 @@ public class HttpRetrieverIntegrationTests {
 
     @Test
     public void retrieves() throws Exception {
-        FilePath target = buildJobWithLibrary(
+        Path target = buildJobWithLibrary(
                 "http-lib-retriever-tests.zip",
                 "1.0"
         );
-        Assert.assertTrue(target.child("version.txt").exists());
-        Assert.assertTrue(target.child("src").exists());
-        Assert.assertTrue(target.child("vars").exists());
-        Assert.assertTrue(target.child("resources").exists());
+        Assert.assertTrue(Files.exists(target.resolve("version.txt")));
+        Assert.assertTrue(Files.exists(target.resolve("src")));
+        Assert.assertTrue(Files.exists(target.resolve("vars")));
+        Assert.assertTrue(Files.exists(target.resolve("resources")));
     }
 
     @Test
     public void retrievesWithPreemptiveAuthEvenIfNotNeeded() throws Exception {
-        FilePath target = buildJobWithLibrary(
+        Path target = buildJobWithLibrary(
                 "http-lib-retriever-tests.zip",
                 "1.0",
                 true
         );
-        Assert.assertTrue(target.child("version.txt").exists());
-        Assert.assertTrue(target.child("src").exists());
-        Assert.assertTrue(target.child("vars").exists());
-        Assert.assertTrue(target.child("resources").exists());
+        Assert.assertTrue(Files.exists(target.resolve("version.txt")));
+        Assert.assertTrue(Files.exists(target.resolve("src")));
+        Assert.assertTrue(Files.exists(target.resolve("vars")));
+        Assert.assertTrue(Files.exists(target.resolve("resources")));
     }
 
     @Test(expected = AssertionError.class)
     public void retrievesRejectedWithoutCredentials() throws Exception {
         ExtensionList.lookupSingleton(SystemCredentialsProvider.class).getCredentials().remove(credentials);
 
-        FilePath target = buildJobWithLibrary(
+        Path target = buildJobWithLibrary(
                 "http-lib-retriever-tests.zip",
                 "1.0"
         );
-        Assert.assertTrue(target.child("version.txt").exists());
-        Assert.assertTrue(target.child("src").exists());
-        Assert.assertTrue(target.child("vars").exists());
-        Assert.assertTrue(target.child("resources").exists());
+        Assert.assertTrue(Files.exists(target.resolve("version.txt")));
+        Assert.assertTrue(Files.exists(target.resolve("src")));
+        Assert.assertTrue(Files.exists(target.resolve("vars")));
+        Assert.assertTrue(Files.exists(target.resolve("resources")));
     }
 
     @Test
     public void acceptsIncorrectVersionsDeclared() throws Exception {
-        FilePath target = buildJobWithLibrary(
+        Path target = buildJobWithLibrary(
                 "http-lib-retriever-tests.zip",
                 "2.3.4"
         );
-        Assert.assertTrue(target.child("version.txt").exists());
-        Assert.assertTrue(target.child("src").exists());
-        Assert.assertTrue(target.child("vars").exists());
-        Assert.assertTrue(target.child("resources").exists());
+        Assert.assertTrue(Files.exists(target.resolve("version.txt")));
+        Assert.assertTrue(Files.exists(target.resolve("src")));
+        Assert.assertTrue(Files.exists(target.resolve("vars")));
+        Assert.assertTrue(Files.exists(target.resolve("resources")));
     }
 
     @Test
     public void acceptsSharedLibEncasedInUpperLevelDirectory() throws Exception {
-        FilePath target = buildJobWithLibrary(
+        Path target = buildJobWithLibrary(
                 "http-lib-retriever-tests-encased-in-upper-directory.zip",
                 "1.2.3"
         );
-        Assert.assertTrue(target.child("version.txt").exists());
-        Assert.assertTrue(target.child("src").exists());
-        Assert.assertTrue(target.child("vars").exists());
-        Assert.assertTrue(target.child("resources").exists());
+        Assert.assertTrue(Files.exists(target.resolve("version.txt")));
+        Assert.assertTrue(Files.exists(target.resolve("src")));
+        Assert.assertTrue(Files.exists(target.resolve("vars")));
+        Assert.assertTrue(Files.exists(target.resolve("resources")));
     }
 
     @Test
     public void acceptsSharedLibraryIfOnlyDirectoryPresentIsVarsSrcOrResources() throws Exception {
-        FilePath target = buildJobWithLibrary(
+        Path target = buildJobWithLibrary(
                 "http-lib-retriever-tests-just-vars-dir.zip",
                 "1.2.3"
         );
-        Assert.assertTrue(target.child("vars").exists());
+        Assert.assertTrue(Files.exists(target.resolve("vars")));
     }
 
     @Test
     public void acceptsNoVersionsDeclared() throws Exception {
-        FilePath target = buildJobWithLibrary(
+        Path target = buildJobWithLibrary(
                 "http-lib-retriever-tests-no-version.zip",
                 "2.3.4"
         );
-        Assert.assertTrue(target.child("src").exists());
-        Assert.assertTrue(target.child("vars").exists());
-        Assert.assertTrue(target.child("resources").exists());
+        Assert.assertTrue(Files.exists(target.resolve("src")));
+        Assert.assertTrue(Files.exists(target.resolve("vars")));
+        Assert.assertTrue(Files.exists(target.resolve("resources")));
     }
 
     @Test
     public void replacesVersionInUrl() throws Exception {
-        FilePath target = buildJobWithLibrary(
+        Path target = buildJobWithLibrary(
                 "http-lib-retriever-test2-1.2.3.zip",
                 "2.3.4"
         );
-        Assert.assertTrue(target.child("src").exists());
-        Assert.assertTrue(target.child("vars").exists());
-        Assert.assertTrue(target.child("resources").exists());
+        Assert.assertTrue(Files.exists(target.resolve("src")));
+        Assert.assertTrue(Files.exists(target.resolve("vars")));
+        Assert.assertTrue(Files.exists(target.resolve("resources")));
     }
 
     @Test
     public void exceptionWhenRetrieveIncorrectUrl() throws Exception {
         boolean buildFailed = false;
         try {
-            FilePath target = buildJobWithLibrary(null, "2.3.4");
-            Assert.assertFalse(target.child("version.txt").exists());
+            Path target = buildJobWithLibrary(null, "2.3.4");
+            Assert.assertFalse(Files.exists(target.resolve("version.txt")));
         } catch (java.lang.AssertionError e) {
             buildFailed = true;
             //e.printStackTrace();
@@ -163,8 +171,8 @@ public class HttpRetrieverIntegrationTests {
     public void doesNotRetrieveIfUrlNotPassed() throws Exception {
         boolean buildFailed = false;
         try {
-            FilePath target = buildJobWithLibrary("http-lib-retriever-tests.zip", "1.2.3", libraryName -> null, false);
-            Assert.assertFalse(target.child("src").exists());
+            Path target = buildJobWithLibrary("http-lib-retriever-tests.zip", "1.2.3", libraryName -> null, false);
+            Assert.assertFalse(Files.exists(target.resolve("src")));
         } catch (java.lang.AssertionError e) {
             buildFailed = true;
             e.printStackTrace();
@@ -172,31 +180,28 @@ public class HttpRetrieverIntegrationTests {
         Assert.assertTrue(buildFailed);
     }
 
-    private @Nonnull
-    FilePath buildJobWithLibrary(@Nullable String fixtureName, @Nullable String libraryVersion)
+    private @NonNull
+    Path buildJobWithLibrary(@Nullable String fixtureName, @Nullable String libraryVersion)
             throws Exception {
         return buildJobWithLibrary(fixtureName, libraryVersion, libraryName -> wireMock.url(libraryName + ".zip"), false);
     }
 
-    private @Nonnull
-    FilePath buildJobWithLibrary(@Nullable String fixtureName, @Nullable String libraryVersion,
-                                 boolean withPreemptiveAuth) throws Exception {
+    private @NonNull
+    Path buildJobWithLibrary(@Nullable String fixtureName, @Nullable String libraryVersion,
+                             boolean withPreemptiveAuth) throws Exception {
         return buildJobWithLibrary(fixtureName, libraryVersion, libraryName -> wireMock.url(libraryName + ".zip"), withPreemptiveAuth);
     }
 
-    private @Nonnull
-    FilePath buildJobWithLibrary(@Nullable String fixtureName, @Nullable String libraryVersion, @Nonnull
+    private @NonNull
+    Path buildJobWithLibrary(@Nullable String fixtureName, @Nullable String libraryVersion, @NonNull
             Function<String, String> urlBuilder, boolean withPreemptiveAuth)
             throws Exception {
-        // FIXME archive name === directory
-
-        String libraryName = "foo";
-
+        Assume.assumeFalse("TODO flakes on Windows due to open file handles", Functions.isWindows());
         String importScript;
         if (libraryVersion != null) {
-            importScript = "@Library('" + libraryName + "@" + libraryVersion + "') _";
+            importScript = "@Library('foo@" + libraryVersion + "') _";
         } else {
-            importScript = "@Library('" + libraryName + "') _";
+            importScript = "@Library('foo') _";
         }
 
         if (fixtureName != null) {
@@ -225,18 +230,23 @@ public class HttpRetrieverIntegrationTests {
                             .willReturn(WireMock.notFound()));
         }
         globalLibraries.getLibraries().add(new LibraryConfiguration(
-                libraryName,
-                new HttpRetriever(urlBuilder.apply(libraryName), credentials.getId(), withPreemptiveAuth)
+                "foo",
+                new HttpRetriever(urlBuilder.apply("foo"), credentials.getId(), withPreemptiveAuth)
         ));
         WorkflowJob p = j.jenkins.createProject(WorkflowJob.class, "p");
         p.setDefinition(new CpsFlowDefinition(importScript, true));
 
         WorkflowRun run = j.buildAndAssertSuccess(p);
-        return new FilePath(run.getRootDir().toPath().resolve("libs").resolve(libraryName).toFile());
+        List<Path> children = Files.list(run.getRootDir().toPath().resolve("libs"))
+                .filter(Files::isDirectory)
+                .collect(Collectors.toList());
+        assertThat(children, hasSize(1));
+        return children.get(0);
     }
 
     @Test
     public void retrievesWithPreemptiveAuthIfNeeded() throws Exception {
+        Assume.assumeFalse("TODO flakes on Windows due to open file handles", Functions.isWindows());
         String libraryName = "foo";
         String importScript = "@Library('foo@1.0') _";
         String fixtureName = "http-lib-retriever-tests.zip";
@@ -261,12 +271,16 @@ public class HttpRetrieverIntegrationTests {
         p.setDefinition(new CpsFlowDefinition(importScript, true));
 
         WorkflowRun run = j.buildAndAssertSuccess(p);
-        FilePath target = new FilePath(run.getRootDir().toPath().resolve("libs").resolve(libraryName).toFile());
+        List<Path> children = Files.list(run.getRootDir().toPath().resolve("libs"))
+                .filter(Files::isDirectory)
+                .collect(Collectors.toList());
+        assertThat(children, hasSize(1));
+        Path target = children.get(0);
 
-        Assert.assertTrue(target.child("version.txt").exists());
-        Assert.assertTrue(target.child("src").exists());
-        Assert.assertTrue(target.child("vars").exists());
-        Assert.assertTrue(target.child("resources").exists());
+        Assert.assertTrue(Files.exists(target.resolve("version.txt")));
+        Assert.assertTrue(Files.exists(target.resolve("src")));
+        Assert.assertTrue(Files.exists(target.resolve("vars")));
+        Assert.assertTrue(Files.exists(target.resolve("resources")));
     }
 
 }
